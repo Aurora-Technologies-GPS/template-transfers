4e7c21b468d4a3e709e4f15d80c0aaae35aab1df
Noel Lora <noellorad@gmail.com>
2024-10-08 15:37:17 -0400

    vista standar 70%

--- src/components/VistaStandar.vue
+++ src/components/VistaStandar.vue
@@ -1,19 +1,25 @@
 <template>
 	<div class="vistaStandar">
-			Hola vista estandar
+		<VistaStandarHeader style="padding: 10px;"/>
+		<VistaStandarList />
 		
 	</div>
 
 </template>
 <script setup>
+	import VistaStandarHeader from './VistaStandarHeader.vue'
+	import VistaStandarList from './VistaStandarList.vue'
 	
 </script>
 <style scoped>
 
 	.vistaStandar{
 		background-color: white;
+		display: flex;
+		flex-direction: column;
+		/*justify-content: space-between;*/
 		height: 100%;
-		padding: 10px;
+		
 	}
 	
 </style>
---
+++ src/components/VistaStandarList.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="vistaStandarList">
+		<div class="contendorTabla">
+
+			<div class="listaTabla">
+				Lista
+			</div>
+
+		</div>
+	</div>
+</template>
+<script setup>
+	
+</script>
+<style scoped>
+
+	.vistaStandarList{
+		height: 100%;
+		width: 100%;
+		overflow: auto;
+		padding: 10px;
+	}
+
+	.contendorTabla{
+		/*padding: 10px;*/
+		height: 100%;
+		overflow: auto;
+
+
+	}
+
+	.listaTabla{
+		border: solid 1px;
+		border-color: #80808030;
+		min-height: 65px;		
+		width: 100%;
+		border-radius: 8px 8px 8px 8px;
+
+	}
+			
+</style>
